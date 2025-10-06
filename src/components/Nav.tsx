2a4f218fa863ca193cab10a7077f7bde136ba5fc
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-10-07 01:22:32 +0800

    feat: implement navigation component with dynamic links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
 export default function Nav() {
-  return <nav></nav>;
+  const pathname = usePathname();
+  return (
+    <nav className="flex gap-8">
+      {links.map((link, idx) => {
+        return (
+          <Link
+            key={idx}
+            href={link.path}
+            className={`${
+              link.path === pathname && "text-accent border-b-2 border-accent"
+            } capitalize font-medium hover:text-accent transition-all`}>
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
 }
