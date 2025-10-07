74fb4b208fdaaee7a06c42ae0a74a142d7256de1
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-10-08 05:03:16 +0800

    feat: add Social component

---
+++ src/components/Social.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  {
+    icons: <FaGithub />,
+    path: "",
+  },
+  {
+    icons: <FaLinkedinIn />,
+    path: "",
+  },
+  {
+    icons: <FaYoutube />,
+    path: "",
+  },
+  {
+    icons: <FaTwitter />,
+    path: "",
+  },
+];
+
+type SocialProps = {
+  containerStyles?: string;
+  iconStyles?: string;
+};
+
+export default function Social({ containerStyles, iconStyles }: SocialProps) {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, idx) => {
+        return (
+          <Link key={idx} href={item.path} className={iconStyles}>
+            {item.icons}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
