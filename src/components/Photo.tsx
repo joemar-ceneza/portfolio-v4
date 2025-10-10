558c78c6ec441b539aeb0296c27ada73e58287c4
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-10-11 04:12:47 +0800

    fix: restructure Photo component for improved layout and accessibility

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -6,11 +6,40 @@ import Image from "next/image";
 export default function Photo() {
   return (
     <div className="w-full h-full relative">
-      <motion.div>
-        <motion.div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
-          <Image src="/asset/profile-picture.png" priority quality={100} fill alt="" className="object-contain"></Image>
-        </motion.div>
+      <motion.div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
+        <Image
+          src="/asset/profile-picture.png"
+          priority
+          quality={100}
+          fill
+          alt="profile-picture"
+          className="object-contain"></Image>
       </motion.div>
+      <motion.svg
+        className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+        fill="transparent"
+        viewBox="0 0 506 506"
+        xmlns="https://www.w3.org/2000/svg">
+        <motion.circle
+          cx="253"
+          cy="253"
+          r="250"
+          stroke="#00ff99"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          initial={{ strokeDasharray: "24 10 0 0" }}
+          animate={{
+            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+            rotate: [120, 360],
+          }}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        />
+      </motion.svg>
     </div>
   );
 }
