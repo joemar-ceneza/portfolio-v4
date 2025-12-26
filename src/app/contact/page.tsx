4e9e8c989599a0d93b6a3ae19b44aec9781c8609
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-12-26 23:20:31 +0800

    feat(contact): update form field names and improve component export

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,7 +14,7 @@ import {
 } from "@/components/ui/select";
 import { contactInfo, services } from "./data";
 
-export default function Services() {
+export default function Contact() {
   return (
     <section className="flex items-center justify-center">
       <div className="container mx-auto">
@@ -27,10 +27,10 @@ export default function Services() {
                 together.
               </p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname" />
-                <Input type="lastname" placeholder="Lastname" />
-                <Input type="email" placeholder="Email Address" />
-                <Input type="phone" placeholder="Phone number" />
+                <Input type="text" name="firstName" placeholder="Firstname" required />
+                <Input type="text" name="lastName" placeholder="Lastname" required />
+                <Input type="email" name="email" placeholder="Email Address" required />
+                <Input type="tel" name="phone" placeholder="Phone number" />
               </div>
               <Select>
                 <SelectTrigger className="w-full">
@@ -47,7 +47,7 @@ export default function Services() {
                   </SelectGroup>
                 </SelectContent>
               </Select>
-              <Textarea className="h-[200px]" placeholder="Type your message here."></Textarea>
+              <Textarea className="h-[200px]" name="message" placeholder="Type your message here."></Textarea>
               <Button size="md" className="max-w-40">
                 Send Message
               </Button>
@@ -55,8 +55,8 @@ export default function Services() {
           </div>
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-6">
-              {contactInfo.map((item, idx) => (
-                <li key={idx} className="flex items-center gap-6">
+              {contactInfo.map((item) => (
+                <li key={item.title} className="flex items-center gap-6">
                   <div className="w-[52px] h-[52px] xl:w-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
                     <div className="text-[28px]">{item.icon}</div>
                   </div>
