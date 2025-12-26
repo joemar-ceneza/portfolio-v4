21fdcf36ea86cb3f26dea303cf9ab6a6e0ec6e49
Joemar Ceneza <joemar.ceneza@gmail.com>
2025-12-26 23:19:21 +0800

    chore: remove unnecessary comment in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
-
-// components
 import Header from "@/components/Header";
 
 const jetbrainsMono = JetBrains_Mono({
